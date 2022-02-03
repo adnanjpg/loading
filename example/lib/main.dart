4cf45a029b99c771e5e3df501217992f15e61098
Adnan <adnanfahedhr@gmail.com>
2022-02-04 00:46:36 +0300

    init example

---
+++ example/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:loading/loading.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: IsLoadingBuilder(
+          builder: (context, isLoading) {
+            return ElevatedButton(
+              onPressed: () {
+                Loading.load(context, !isLoading);
+              },
+              child: Text(isLoading ? 'Loading' : 'Load'),
+            );
+          },
+        ),
+      ),
+      body: const Loading(
+        child: Center(
+          child: Text('aha'),
+        ),
+      ),
+    );
+  }
+}
